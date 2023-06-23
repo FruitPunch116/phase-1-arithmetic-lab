fcb10ba103815524b3dcb17f934af48bd2dc4302
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-06-22 21:00:46 -0400

    explain the code process

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+//All the logs to the console are for the testing process.
 
-// Write your code here
 let num1 = 31;
 let num2 = 2;
+
+//alternative method
 // const multiply = num1 * num2;
+
+//Multiply a random number between 0 + (31 * 2)
 const multiply = Math.round(Math.random()*0 +(num1 * num2));
-console.log(`"Multiply" result: ${multiply}`)
+// console.log(`"Multiply" result: ${multiply}`)
 
+//Generate a random number between 0 and 100.
 const random = Math.round(Math.random()*100)
-console.log(`"Random" result: ${random}`)
-
-//Do not work, create a bug, and breaks the npm tests.
-// console.log(`This is math random ${Math.random()}`)
+// console.log(`"Random" result: ${random}`)
 
 let num3 = 28;
 let num4 = 6;
+//Return the leftover from the divission.
 const mod = num3%num4
-console.log(`"Mod" result: ${mod}`)
+// console.log(`"Mod" result: ${mod}`)
 
 let min = 19;
 const max = Math.floor(Math.random()*(min - min)+20)
-console.log(`"Max" result: ${max}`)
+// console.log(`"Max" result: ${max}`)
